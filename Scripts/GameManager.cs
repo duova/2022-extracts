9a4cc5547a816c8b2044cf2d58c7d7f194f57fe8
duova <61954674+duova@users.noreply.github.com>
2022-12-22 23:13:50 +0800

    Add files via upload

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool inverted;
+    public bool resettingLevel;
+    public int levelResetCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (resettingLevel == true)
+        {
+            levelResetCounter++;
+            if (levelResetCounter >= 10)
+            {
+                resettingLevel = false;
+                levelResetCounter = 0;
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// only called by HealthRespawn class, ignore otherwise
+    /// </summary>
+    public void ResetLevel()
+    {
+        resettingLevel = true;
+    }
+}
